e1c246644e7a5b01d63cd76b02b90f43c69b73ed
dipanc1 <dipanchhabra@gmail.com>
2024-04-03 22:30:00 +0530

    Remove LinkedIn component and related files

--- components/Create/components/index.js
+++ components/Create/components/index.js
@@ -1,9 +1,7 @@
 import DragAndDrop from './DragAndDrop';
 import CopyPaste from './CopyPaste';
-import LinkedIn from './LinkedIn';
 
 export {
   DragAndDrop,
-  CopyPaste,
-  LinkedIn
+  CopyPaste
 };
