93b8db1635969878580de01eb2f61dab303e8c37
tradestealer999 <112725984+truerep@users.noreply.github.com>
2024-04-06 14:33:48 +0530

    Added analytics

--- pages/_app.js
+++ pages/_app.js
@@ -1,8 +1,7 @@
+/* eslint-disable react/no-danger */
 /* eslint-disable react/react-in-jsx-scope */
-import {
-  authorizedRoutes
-} from '@/constants';
 import '@/styles/globals.css';
+import Head from 'next/head';
 import {
   useRouter
 } from 'next/router';
@@ -14,6 +13,10 @@ import {
   Toaster
 } from 'react-hot-toast';
 import 'react-tippy/dist/tippy.css';
+
+import {
+  authorizedRoutes
+} from '@/constants';
 
 export default function App({Component, pageProps}) {
   const router = useRouter();
@@ -42,6 +45,22 @@ export default function App({Component, pageProps}) {
         position="top-right"
         reverseOrder={false}
       />
+      <Head>
+        <script
+          async
+          src="https://www.googletagmanager.com/gtag/js?id=G-N275J762K6"
+        />
+        <script
+          dangerouslySetInnerHTML={{
+            __html: `
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+            gtag('config', 'G-N275J762K6');
+            `
+          }}
+        />
+      </Head>
       <Component {...pageProps} />
     </>
   );
