df644b032d047077ada1d73b560be598510a8f37
dipanc1 <dipanchhabra@gmail.com>
2024-05-13 22:36:27 +0530

    chore: Update templateKeys import and usage in TemplateRender and ResumeContext

--- helpers/templateKeys.js
+++ helpers/templateKeys.js
@@ -1,11 +1,8 @@
-export const MODERN_PRISM = 'modern-prism';
-export const MINIMALISTIC_BASIC = 'minimalistic-basic';
-export const MODERN = 'modern';
-export const TECH_STARTER = 'tech-starter';
-
-export const templateKeys = {
-    MODERN_PRISM,
-    MINIMALISTIC_BASIC,
-    MODERN,
-    TECH_STARTER
+const templateKeys = {
+    MODERN_PRISM: 'modern-prism',
+    MINIMALISTIC_BASIC: 'minimalistic-basic',
+    MODERN: 'modern',
+    TECH_STARTER: 'tech-starter'
 };
+
+export default templateKeys;
