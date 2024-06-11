5e87213d1300ac6743cf6ee43baeda6c9cf0ffe8
dipanc1 <dipanchhabra@gmail.com>
2024-06-11 09:07:03 +0530

    reading extension text

--- components/Create/components/CopyPaste/CopyPaste.container.js
+++ components/Create/components/CopyPaste/CopyPaste.container.js
@@ -1,16 +1,16 @@
 import React, {
+  useEffect,
+  useRef,
   useState
 } from 'react';
 import {
   useRouter
 } from 'next/router';
 import CopyPaste from './CopyPaste';
-import {
-  getResumeText
-} from '@/api';
 
 const CopyPasteContainer = () => {
   const [resumeText, setResumeText] = useState('');
+  const textAreaRef = useRef(null);
 
   const router = useRouter();
 
@@ -18,14 +18,24 @@ const CopyPasteContainer = () => {
     try {
       sessionStorage.setItem('resumeString', resumeText);
       router.push('/builder');
-    } catch (err) {}
+    } catch (err) { }
   };
+
+  useEffect(() => {
+    if (router.query?.tab === 'copy-paste') {
+      textAreaRef && textAreaRef.current?.focus()
+      navigator.clipboard.readText().then((text) => {
+        setResumeText(text);
+      });
+    }
+  }, [router.query]);
 
   return (
     <CopyPaste
       resumeText={resumeText}
       setResumeText={setResumeText}
       handleText={handleText}
+      ref={textAreaRef}
     />
   );
 };
