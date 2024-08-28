e798c6df9eb9f32907dd19df912868164567bf60
dipanc1 <dipanchhabra@gmail.com>
2024-08-28 16:14:02 +0530

    chore: Refactor FAQs container to use dynamic state for toggle functionality

--- components/NewHome/components/FAQs/FAQs.container.js
+++ components/NewHome/components/FAQs/FAQs.container.js
@@ -1,73 +1,60 @@
-import React from 'react'
-import FAQs from './FAQs'
+import React from 'react';
+import FAQs from './FAQs';
 
 const FAQsContainer = () => {
+  const QAsLength = 4;
 
-  const toggleAnswer1 = () => {
-    setShowAnswer1(!showAnswer1);
-    setShowAnswer2(false);
-    setShowAnswer3(false);
-    setShowAnswer4(false);
-  }
+  const [state, setState] = React.useState(() => {
+    const initialState = {};
+    for (let i = 1; i <= QAsLength; i++) {
+      initialState[`showAnswer${i}`] = false;
+    }
+    return initialState;
+  });
 
-  const toggleAnswer2 = () => {
-    setShowAnswer2(!showAnswer2);
-    setShowAnswer1(false);
-    setShowAnswer3(false);
-    setShowAnswer4(false);
-  }
+  const toggleAnswer = (index) => {
+    setState((prevState) => {
+      const updatedState = {};
+      for (let i = 1; i <= QAsLength; i++) {
+        updatedState[`showAnswer${i}`] = i === index ? !prevState[`showAnswer${i}`] : false;
+      }
+      return updatedState;
+    });
+  };
 
-  const toggleAnswer3 = () => {
-    setShowAnswer3(!showAnswer3);
-    setShowAnswer1(false);
-    setShowAnswer2(false);
-    setShowAnswer4(false);
-  }
-
-  const toggleAnswer4 = () => {
-    setShowAnswer4(!showAnswer4);
-    setShowAnswer1(false);
-    setShowAnswer2(false);
-    setShowAnswer3(false);
-  }
-
-  const [showAnswer1, setShowAnswer1] = React.useState(false);
-  const [showAnswer2, setShowAnswer2] = React.useState(false);
-  const [showAnswer3, setShowAnswer3] = React.useState(false);
-  const [showAnswer4, setShowAnswer4] = React.useState(false);
-
+  // change Qas length at the top after adding more questions
   const QAs = [
     {
       question: 'What is AICVPro?',
       answer: 'AICVPro is a platform that provides services help you build your CV with the help of AI. You can upload your CV and we will generate a new CV for you based on the information you provide. You can also choose from a variety of templates and customize your CV to your liking. You can also customuze your CV with job descriptions and we will generate a CV for you based on the job description you provide.',
-      showAnswer: showAnswer1,
-      toggleAnswer: toggleAnswer1
+      showAnswer: state.showAnswer1,
+      toggleAnswer: () => toggleAnswer(1)
     },
     {
       question: 'Is AICVPro free to use?',
       answer: 'Yes, AICVPro is free to use. You can upload your CV and generate a new CV for free. You can also choose from a variety of templates and customize your CV to your liking for free. You can also customize your CV with job descriptions and we will generate a CV for you based on the job description you provide for free.',
-      showAnswer: showAnswer2,
-      toggleAnswer: toggleAnswer2
+      showAnswer: state.showAnswer2,
+      toggleAnswer: () => toggleAnswer(2)
     },
     {
       question: 'What makes AICVPro different from other CV building platforms?',
       answer: 'AICVPro is different from other CV building platforms because we use AI to generate a new CV for you based on the information you provide. You can also choose from a variety of templates and customize your CV to your liking. You can also customize your CV with job descriptions and we will generate a CV for you based on the job description you provide. AICVPro is also free to use.',
-      showAnswer: showAnswer3,
-      toggleAnswer: toggleAnswer3
+      showAnswer: state.showAnswer3,
+      toggleAnswer: () => toggleAnswer(3)
     },
     {
       question: 'Can I save my CV on AICVPro?',
       answer: 'Yes, you can save your CV on AICVPro. You can also download your CV as a PDF file and share it with potential employers. You can also edit your CV and generate a new CV based on the information you provide.',
-      showAnswer: showAnswer4,
-      toggleAnswer: toggleAnswer4
+      showAnswer: state.showAnswer4,
+      toggleAnswer: () => toggleAnswer(4)
     }
-  ]
+  ];
 
   return (
     <FAQs
       QAs={QAs}
     />
-  )
-}
+  );
+};
 
-export default FAQsContainer
+export default FAQsContainer;
