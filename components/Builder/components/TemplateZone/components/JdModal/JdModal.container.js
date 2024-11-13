7e7b2e8157489938a18be77a0042dd39601b9a20
dipanc1 <dipanchhabra@gmail.com>
2024-11-13 19:42:50 +0530

    refactor resume modification logic in SyncJobDescriptionModal and JdModal for improved error handling and state management

--- components/Builder/components/TemplateZone/components/JdModal/JdModal.container.js
+++ components/Builder/components/TemplateZone/components/JdModal/JdModal.container.js
@@ -18,34 +18,35 @@ const JdModalContainer = () => {
   const { resumeData, updateResumeData } = useContext(ResumeContext);
 
   const modifyResume = async () => {
+    if (!jobDescription.length || !resumeData?.resumeString) return;
+
     try {
       setIsLoading(true);
-      toast.loading('Modifying resume...', {
-        id: 'modifying-resume'
-      });
-      if (jobDescription.length > 0 && resumeData?.resumeString) {
-        const textForAI = prompt ? prompt + " " + jobDescription : jobDescription;
-        
-        const res = await getModifiedResume(resumeData.resumeString, textForAI);
-        if (res?.basics) {
-          toast.success('Resume modified!', {
-            id: 'modifying-resume'
-          });
-          updateResumeData((prevState) => {
-            return {
-              ...prevState,
-              toggleJdModal: false,
-              templateData: res
-            };
-          });
-          setPrompt('');
-          setJobDescription('');
-        }
-      } else { }
+      toast.loading('Modifying resume...', { id: 'modifying-resume' });
+
+      const textForAI = prompt ? `${prompt} ${jobDescription}` : jobDescription;
+      const res = await getModifiedResume(resumeData.resumeString, textForAI);
+
+      if (res?.basics) {
+        toast.success('Resume modified!', { id: 'modifying-resume' });
+
+        const storedResumeString = window?.sessionStorage?.getItem('resumeString');
+        const url = JSON.parse(storedResumeString)?.url;
+
+        window.sessionStorage.setItem('resumeString', JSON.stringify({ url, text: res }));
+
+        updateResumeData(prevState => ({
+          ...prevState,
+          toggleJdModal: false,
+          resumeString: JSON.stringify(res),
+          templateData: res
+        }));
+
+        setPrompt('');
+        setJobDescription('');
+      }
     } catch (err) {
-      toast.error(err?.response?.data?.error ?? 'Error modifying resume!', {
-        id: 'modifying-resume'
-      });
+      toast.error(err?.response?.data?.error ?? 'Error modifying resume!', { id: 'modifying-resume' });
     } finally {
       setIsLoading(false);
     }
