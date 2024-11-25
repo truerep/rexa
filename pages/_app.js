da5ec0abdfa387437535c2e3d4975f6e366d9b81
dipanc1 <dipanchhabra@gmail.com>
2024-11-25 21:40:46 +0530

    feat: integrate Microsoft Clarity tracking script in _app.js

--- pages/_app.js
+++ pages/_app.js
@@ -36,6 +36,17 @@ export default function App({Component, pageProps}) {
             `
           }}
         />
+        <script
+          dangerouslySetInnerHTML={{
+            __html: `
+            (function(c,l,a,r,i,t,y){
+                c[a]=c[a]||function(){(c[a].q=c[a].q||[]).push(arguments)};
+                t=l.createElement(r);t.async=1;t.src="https://www.clarity.ms/tag/"+i;
+                y=l.getElementsByTagName(r)[0];y.parentNode.insertBefore(t,y);
+            })(window, document, "clarity", "script", "p45saqdofl");
+            `
+          }}
+        />
       </Head>
       <Component {...pageProps} />
     </>
